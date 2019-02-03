29480fb60d3fa9445cc9e8e0a390a421017b2642
Mike West <westmd23@gmail.com>
2019-02-03 17:01:41 -0500

    vuejs exercises

---
+++ exercise_2/app.js
@@ -0,0 +1,11 @@
+new Vue({
+        el: '#exercise',
+        data: {
+            value: ''
+        },
+        methods: {
+            talk: function() {
+                alert("I've been clicked!!")
+            }
+        }
+    });
